72ad67cc7bebb45d3aa8980926d0fb9f16f346b1
Sidd9840 <siddu17022002@gmail.com>
2026-06-10 21:37:10 +0530

    Update addStudent.js

--- JS/addStudent.js
+++ JS/addStudent.js
@@ -1,105 +1,24 @@
 document.getElementById("studentForm")
-.addEventListener("submit", function (e)
-{
+.addEventListener("submit", function(e){
+
     e.preventDefault();
 
-    var name =
-    document.querySelector('input[placeholder="Student Name"]').value;
-
-    var roll =
-    document.querySelector('input[placeholder="Roll Number"]').value;
-
-    var course =
-    document.querySelector('input[placeholder="Course"]').value;
-
-    var email =
-    document.querySelector('input[placeholder="Email"]').value;
-
-    var students =
-    JSON.parse(localStorage.getItem("students")) || [];
-
-    // Duplicate Roll Check
-    for(let i = 0; i < students.length; i++)
-    {
-        if(students[i].roll === roll)
-        {
-            alert("Student Already Exists");
-            return;
-        }
-    }
-
-    var student = {
-        name: name,
-        roll: roll,
-        course: course,
-        email: email
+    let student = {
+        name: document.getElementById("studentName").value,
+        roll: document.getElementById("roll").value,
+        course: document.getElementById("course").value,
+        email: document.getElementById("email").value
     };
+
+    let students =
+    JSON.parse(localStorage.getItem("students")) || [];
 
     students.push(student);
 
-    localStorage.setItem(
-        "students",
-        JSON.stringify(students)
-    );
+    localStorage.setItem("students", JSON.stringify(students));
 
     alert("Student Added Successfully");
 
     document.getElementById("studentForm").reset();
 
-    showStudents();
 });
-
-
-function showStudents()
-{
-    var students =
-    JSON.parse(localStorage.getItem("students")) || [];
-
-    var table =
-    document.getElementById("studentTable");
-
-    table.innerHTML = `
-    <tr>
-        <th>Roll No</th>
-        <th>Name</th>
-        <th>Course</th>
-        <th>Email</th>
-        <th>Action</th>
-    </tr>
-    `;
-
-    students.forEach(function (s, index)
-    {
-        table.innerHTML += `
-        <tr>
-            <td>${s.roll}</td>
-            <td>${s.name}</td>
-            <td>${s.course}</td>
-            <td>${s.email}</td>
-            <td>
-                <button onclick="deleteStudent(${index})">
-                Delete
-                </button>
-            </td>
-        </tr>
-        `;
-    });
-}
-
-showStudents();
-
-
-function deleteStudent(index)
-{
-    var students =
-    JSON.parse(localStorage.getItem("students")) || [];
-
-    students.splice(index, 1);
-
-    localStorage.setItem(
-        "students",
-        JSON.stringify(students)
-    );
-
-    showStudents();
-}
