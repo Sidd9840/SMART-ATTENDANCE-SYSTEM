5cd52ef97698a08ddac1f06d6db843dab76fc642
Sidd9840 <siddu17022002@gmail.com>
2026-06-10 21:54:59 +0530

    Update studentList.js

--- JS/studentList.js
+++ JS/studentList.js
@@ -2,16 +2,15 @@ let students =
 JSON.parse(localStorage.getItem("students")) || [];
 
 let table =
-document.getElementById("studentListTable");
+document.getElementById("studentTable");
 
-students.forEach(function(s)
-{
-    table.innerHTML += `
-    <tr>
-        <td>${s.roll}</td>
-        <td>${s.name}</td>
-        <td>${s.course}</td>
-        <td>${s.email}</td>
-    </tr>
-    `;
+students.forEach(function(student){
+
+    let row = table.insertRow();
+
+    row.insertCell(0).innerHTML = student.name;
+    row.insertCell(1).innerHTML = student.roll;
+    row.insertCell(2).innerHTML = student.course;
+    row.insertCell(3).innerHTML = student.email;
+
 });
