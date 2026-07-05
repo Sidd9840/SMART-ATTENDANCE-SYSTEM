d01fa394f94572c0f3a155d4a7f9abc8dd20a913
Sidd9840 <siddu17022002@gmail.com>
2026-07-05 18:22:14 +0530

    Update studentLogin.js

--- JS/studentLogin.js
+++ JS/studentLogin.js
@@ -1,33 +1,79 @@
-function studentLogin()
-{
-    let roll = document.getElementById("studentRoll").value;
+// Demo Students (Only First Time)
 
-    let password =
-    document.getElementById("studentPassword").value;
+let students = JSON.parse(localStorage.getItem("students"));
 
-    let students =
-    JSON.parse(localStorage.getItem("students")) || [];
+if (!students) {
 
-    let validUser = false;
+    students = [
 
-    students.forEach(function(student)
-    {
-        if(student.roll === roll &&
-           student.password === password)
         {
-            validUser = true;
+            name: "Siddharth Singh Tomar",
+            roll: "101",
+            password: "123456"
+        },
+
+        {
+            name: "Barnali",
+            roll: "102",
+            password: "123456"
+        },
+
+        {
+            name: "Aakriti",
+            roll: "103",
+            password: "123456"
+        },
+
+        {
+            name: "Sonali",
+            roll: "104",
+            password: "123456"
+        },
+
+        {
+            name: "Nikhil",
+            roll: "105",
+            password: "123456"
         }
+
+    ];
+
+    localStorage.setItem("students", JSON.stringify(students));
+
+}
+
+// Student Login
+
+function studentLogin() {
+
+    let roll = document.getElementById("studentRoll").value.trim();
+
+    let password = document.getElementById("studentPassword").value.trim();
+
+    let students = JSON.parse(localStorage.getItem("students")) || [];
+
+    let validUser = students.find(function(student) {
+
+        return student.roll === roll &&
+               student.password === password;
+
     });
 
-    if(validUser)
-    {
-        window.location.href =
-        "markAttendance.html";
-    }
-    else
-    {
-        alert("Invalid Login");
+    if (validUser) {
+
+        // Logged-in Student Save
+        localStorage.setItem("loggedInStudent", JSON.stringify(validUser));
+
+        alert("Login Successful");
+
+        window.location.href = "markAttendance.html";
+
+    } else {
+
+        alert("Invalid Roll Number or Password");
+
     }
 
     return false;
+
 }
