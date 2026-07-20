c2d5507a275cd745674b5075fc1f9d802d7dc4f2
Sidd9840 <siddu17022002@gmail.com>
2026-07-21 04:59:16 +0530

    Update dashboard.js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,26 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Logout button
-    const logoutBtn = document.querySelector('a[href="index.html"]');
+    // Dashboard Data
+    fetch("http://localhost:8080/dashboard")
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById("totalStudents").innerText =
+        data.totalStudents;
+
+        document.getElementById("totalTeachers").innerText =
+        data.totalTeachers;
+
+        document.getElementById("totalAttendance").innerText =
+        data.totalAttendance;
+
+        document.getElementById("present").innerText =
+        data.present;
+
+        document.getElementById("absent").innerText =
+        data.absent;
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    // Logout
+    const logoutBtn = document.querySelector('a[href="../index.html"]');
 
     logoutBtn.addEventListener("click", function (e) {
 
         let confirmLogout = confirm("Are you sure you want to logout?");
 
         if (!confirmLogout) {
-            e.preventDefault(); // logout cancel
+            e.preventDefault();
         }
 
+        localStorage.removeItem("teacher");
+
     });
-
-});
-
-document.addEventListener("DOMContentLoaded", function(){
-
-    var students = JSON.parse(localStorage.getItem("students")) || [];
-
-    var total = students.length;
-
-    document.getElementById("totalStudents").innerText = "Total Students : " + total;
 
 });
