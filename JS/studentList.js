c8e71630e098ad3e0cf5416b2a136e5fcf7a15e8
Sidd9840 <siddu17022002@gmail.com>
2026-07-21 01:28:41 +0530

    Update studentList.js

--- JS/studentList.js
+++ JS/studentList.js
@@ -13,9 +13,44 @@ fetch("http://localhost:8080/students")
         row.insertCell(2).innerHTML = student.course;
         row.insertCell(3).innerHTML = student.email;
 
+        row.insertCell(4).innerHTML =
+        `<button onclick="deleteStudent(${student.id})">
+            Delete
+        </button>`;
+
     });
 
 })
 .catch(error => {
     console.error(error);
 });
+
+function deleteStudent(id){
+
+    if(confirm("Are you sure you want to delete this student?")){
+
+        fetch("http://localhost:8080/students/" + id,{
+
+            method:"DELETE"
+
+        })
+
+        .then(response => response.text())
+
+        .then(message=>{
+
+            alert(message);
+
+            location.reload();
+
+        })
+
+        .catch(error=>{
+
+            console.error(error);
+
+        });
+
+    }
+
+}
