227610c4c963717248317d06631de9ab026faf79
Sidd9840 <siddu17022002@gmail.com>
2026-07-21 00:45:13 +0530

    Update studentList.js

--- JS/studentList.js
+++ JS/studentList.js
@@ -1,16 +1,21 @@
-let students =
-JSON.parse(localStorage.getItem("students")) || [];
+fetch("http://localhost:8080/students")
+.then(response => response.json())
+.then(students => {
 
-let table =
-document.getElementById("studentTable");
+    let table = document.getElementById("studentTable");
 
-students.forEach(function(student){
+    students.forEach(function(student){
 
-    let row = table.insertRow();
+        let row = table.insertRow();
 
-    row.insertCell(0).innerHTML = student.name;
-    row.insertCell(1).innerHTML = student.roll;
-    row.insertCell(2).innerHTML = student.course;
-    row.insertCell(3).innerHTML = student.email;
+        row.insertCell(0).innerHTML = student.name;
+        row.insertCell(1).innerHTML = student.rollNo;
+        row.insertCell(2).innerHTML = student.course;
+        row.insertCell(3).innerHTML = student.email;
 
+    });
+
+})
+.catch(error => {
+    console.error(error);
 });
