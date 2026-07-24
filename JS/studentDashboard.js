116014373d7614c0f11ccbc234f24989342768a1
Sidd9840 <siddu17022002@gmail.com>
2026-07-24 14:18:20 +0530

    Update studentDashboard.js

--- JS/studentDashboard.js
+++ JS/studentDashboard.js
@@ -1,24 +1,36 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    let student =
-    JSON.parse(localStorage.getItem("student"));
+    // Logged In Student
+    let student = JSON.parse(localStorage.getItem("student"));
 
-    if(student == null){
+    if (student == null) {
 
-        window.location.href =
-        "studentLogin.html";
-
+        window.location.href = "studentLogin.html";
         return;
 
     }
 
+    // Student Details
     document.getElementById("studentName").innerText =
-    student.name;
+        student.name;
 
     document.getElementById("rollNo").innerText =
-    student.rollNo;
+        student.rollNo;
 
     document.getElementById("course").innerText =
-    student.course;
+        student.course;
 
-});
+    // Attendance Summary
+    fetch("http://localhost:8080/attendance/percentage/" + student.id)
+
+        .then(response => response.json())
+
+        .then(data => {
+
+            document.getElementById("present").innerText =
+                data.present;
+
+            document.getElementById("total").innerText =
+                data.total;
+
+            document.getElementById("
