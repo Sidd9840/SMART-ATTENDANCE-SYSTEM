c092d56e9667ceb6df7a727a473d7c5ddc03e85d
Sidd9840 <siddu17022002@gmail.com>
2026-07-24 09:58:25 +0530

    Update studentAttendance.js

--- JS/studentAttendance.js
+++ JS/studentAttendance.js
@@ -1,1 +1,47 @@
+// Logged In Student
+let student = JSON.parse(localStorage.getItem("student"));
 
+if(student == null){
+
+    alert("Please Login First");
+
+    window.location.href="studentLogin.html";
+
+}
+
+// Load Student Attendance
+
+fetch("http://localhost:8080/attendance/student/" + student.id)
+
+.then(response => response.json())
+
+.then(data=>{
+
+    let table =
+    document.getElementById("attendanceTable");
+
+    data.forEach(function(record){
+
+        let row =
+        table.insertRow();
+
+        row.insertCell(0).innerHTML =
+        record.attendanceDate;
+
+        row.insertCell(1).innerHTML =
+        record.subject;
+
+        row.insertCell(2).innerHTML =
+        record.status;
+
+    });
+
+})
+
+.catch(error=>{
+
+    console.log(error);
+
+    alert("Unable to Load Attendance");
+
+});
