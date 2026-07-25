bcf5d7df106333f0e72722c03e50581ff5d99d9c
Sidd9840 <siddu17022002@gmail.com>
2026-07-25 09:15:01 +0530

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,1 +1,28 @@
+function login(){
 
+    let role =
+    document.getElementById("role").value;
+
+    if(role==""){
+
+        alert("Please Select Role");
+
+        return;
+
+    }
+
+    if(role=="Student"){
+
+        window.location.href =
+        "studentLogin.html";
+
+    }
+
+    else{
+
+        window.location.href =
+        "teacherLogin.html";
+
+    }
+
+}
