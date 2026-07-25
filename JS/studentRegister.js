26c4ea463fadb1c799cd3cc266a216ec044ec120
Sidd9840 <siddu17022002@gmail.com>
2026-07-25 09:19:30 +0530

    Update studentRegister.js

--- JS/studentRegister.js
+++ JS/studentRegister.js
@@ -1,1 +1,58 @@
+let data =
+JSON.parse(localStorage.getItem("registerData"));
 
+document.getElementById("name").value =
+data.name;
+
+document.getElementById("email").value =
+data.email;
+
+function registerStudent(){
+
+    let student={
+
+        name:data.name,
+
+        rollNo:document.getElementById("rollNo").value,
+
+        course:document.getElementById("course").value,
+
+        email:data.email,
+
+        password:data.password
+
+    };
+
+    fetch("http://localhost:8080/student/register",{
+
+        method:"POST",
+
+        headers:{
+
+            "Content-Type":"application/json"
+
+        },
+
+        body:JSON.stringify(student)
+
+    })
+
+    .then(response=>response.text())
+
+    .then(message=>{
+
+        alert(message);
+
+        localStorage.removeItem("registerData");
+
+        window.location.href="login.html";
+
+    })
+
+    .catch(error=>{
+
+        console.log(error);
+
+    });
+
+}
