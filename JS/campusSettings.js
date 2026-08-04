6d66c623de250f54a9953a7221c9f6c5a80efbe0
Sidd9840 <siddu17022002@gmail.com>
2026-08-04 17:31:05 +0530

    Update campusSettings.js

--- JS/campusSettings.js
+++ JS/campusSettings.js
@@ -1,93 +1,148 @@
-// -------------------------------------
-// Load Current Campus Location
-// -------------------------------------
+window.onload = function () {
 
-window.onload = function(){
-
-    fetch("http://localhost:8080/campus-location")
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        if(data != null){
-
-            document.getElementById("campusName").value =
-            data.campusName;
-
-            document.getElementById("allowedDistance").value =
-            data.allowedDistance;
-
-        }
-
-    })
-
-    .catch(error => {
-
-        console.log(error);
-
-        alert("Unable to load campus settings.");
-
-    });
+    loadCampus();
 
 };
 
-// -------------------------------------
-// Save Campus Settings
-// -------------------------------------
+// -----------------------------
+// Load Campus
+// -----------------------------
+function loadCampus() {
 
-function saveCampusLocation(){
-
-    let campusLocation = {
-
-        campusName:
-        document.getElementById("campusName").value,
-
-        allowedDistance:
-        parseFloat(
-            document.getElementById("allowedDistance").value
-        )
-
-    };
-
-    fetch("http://localhost:8080/campus-location",{
-
-        method:"PUT",
-
-        headers:{
-
-            "Content-Type":"application/json"
-
-        },
-
-        body:JSON.stringify(campusLocation)
-
-    })
+    fetch("http://localhost:8080/campus")
 
     .then(response => response.json())
 
     .then(data => {
 
-        alert("Campus Settings Updated Successfully.");
+        if(data == null){
+
+            return;
+
+        }
+
+        document.getElementById("campusName").value =
+        data.campusName;
+
+        document.getElementById("latitude").value =
+        data.latitude;
+
+        document.getElementById("longitude").value =
+        data.longitude;
+
+        document.getElementById("allowedDistance").value =
+        data.allowedDistance;
 
     })
 
     .catch(error => {
 
         console.log(error);
-
-        alert("Unable to update campus settings.");
 
     });
 
 }
 
-// -------------------------------------
-// Back
-// -------------------------------------
+// -----------------------------
+// Save Campus
+// -----------------------------
+function saveCampus() {
 
-function goBack(){
+    let campus = {
 
-    window.location.href = "adminDashboard.html";
+        campusName:
+        document.getElementById("campusName").value,
+
+        latitude:
+        document.getElementById("latitude").value,
+
+        longitude:
+        document.getElementById("longitude").value,
+
+        allowedDistance:
+        document.getElementById("allowedDistance").value
+
+    };
+
+    fetch("http://localhost:8080/campus", {
+
+        method: "POST",
+
+        headers: {
+
+            "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify(campus)
+
+    })
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        alert("Campus Saved Successfully");
+
+    })
+
+    .catch(error => {
+
+        console.log(error);
+
+    });
+
+}
+
+// -----------------------------
+// Update Campus
+// -----------------------------
+function updateCampus() {
+
+    let campus = {
+
+        id:1,
+
+        campusName:
+        document.getElementById("campusName").value,
+
+        latitude:
+        document.getElementById("latitude").value,
+
+        longitude:
+        document.getElementById("longitude").value,
+
+        allowedDistance:
+        document.getElementById("allowedDistance").value
+
+    };
+
+    fetch("http://localhost:8080/campus", {
+
+        method: "PUT",
+
+        headers: {
+
+            "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify(campus)
+
+    })
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        alert("Campus Updated Successfully");
+
+    })
+
+    .catch(error => {
+
+        console.log(error);
+
+    });
 
 }
